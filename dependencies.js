ec277b8074a8270189746ab2edefb68fb8bd72ff
Seth Holladay <me@seth-holladay.com>
2019-06-11 22:13:02 -0400

    Update dependencies

--- dependencies.js
+++ dependencies.js
@@ -1,3 +1,3 @@
-export * as http from 'https://deno.land/x/http@v0.3.4/server.ts';
-export { Status as status } from 'https://deno.land/x/http@v0.3.4/http_status.ts';
-export { STATUS_TEXT as statusText } from 'https://deno.land/x/http@v0.3.4/http_status.ts';
+export * as http from 'https://deno.land/x/http@v0.8.0/server.ts';
+export { Status as status } from 'https://deno.land/x/http@v0.8.0/http_status.ts';
+export { STATUS_TEXT as statusText } from 'https://deno.land/x/http@v0.8.0/http_status.ts';
