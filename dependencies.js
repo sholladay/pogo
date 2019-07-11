d5df0d2c6a221d42c35500a7466c46f66da5d794
Hampton <me@hampton.pw>
2019-07-11 07:46:18 +0200

    Update stdlib URL imports
    * Fixed Issue #3 where failed to import due to the http library changing
    location
    
    * Change Travis version of deno

--- dependencies.js
+++ dependencies.js
@@ -1,3 +1,5 @@
-export * as http from 'https://deno.land/x/http@v0.8.0/server.ts';
-export { Status as status } from 'https://deno.land/x/http@v0.8.0/http_status.ts';
-export { STATUS_TEXT as statusText } from 'https://deno.land/x/http@v0.8.0/http_status.ts';
+import * as http from "https://deno.land/std@v0.11.0/http/server.ts";
+import { Status as status } from "https://deno.land/std@v0.11.0/http/http_status.ts";
+import { STATUS_TEXT as statusText } from "https://deno.land/std@v0.11.0/http/http_status.ts";
+
+export { http, status, statusText };
