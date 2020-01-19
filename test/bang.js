de89fdecea0963cacdaba0464d5db78d4538c45c
Seth Holladay <me@seth-holladay.com>
2020-01-19 04:17:31 -0500

    Move Bang message to its own argument

--- test/bang.js
+++ test/bang.js
@@ -2,19 +2,76 @@ import { assert, assertEquals, assertStrictEq, test } from '../dev-dependencies.
 import Response from '../lib/response.js';
 import * as bang from '../lib/bang.js';
 
-test('new Bang() is an instance of Error and Bang', () => {
+test('new Bang() is a 500 error by default', () => {
     const error = new bang.Bang();
     assert(error instanceof Error);
     assert(error instanceof bang.Bang);
-});
-
-test('new Bang() is a 500 error by default', () => {
-    const error = new bang.Bang();
+    assertStrictEq(error.message, 'Internal Server Error');
     assert(error.response instanceof Response);
     assertStrictEq(error.response.status, 500);
     assertEquals(error.response.body, {
         error   : 'Internal Server Error',
         message : 'Internal Server Error',
         status  : 500
+    });
+});
+
+test('new Bang() with custom message', () => {
+    const error = new bang.Bang('some problem');
+    assert(error instanceof Error);
+    assertStrictEq(error.message, 'some problem');
+    assertStrictEq(error.response.status, 500);
+    assertEquals(error.response.body, {
+        error   : 'Internal Server Error',
+        message : 'Internal Server Error',
+        status  : 500
+    });
+});
+
+test('new Bang() with custom message and explicit 500 status', () => {
+    const error = new bang.Bang('some problem', { status : 500 });
+    assert(error instanceof Error);
+    assertStrictEq(error.message, 'some problem');
+    assertStrictEq(error.response.status, 500);
+    assertEquals(error.response.body, {
+        error   : 'Internal Server Error',
+        message : 'Internal Server Error',
+        status  : 500
+    });
+});
+
+test('new Bang() with custom message and 501 status', () => {
+    const error = new bang.Bang('some problem', { status : 501 });
+    assert(error instanceof Error);
+    assertStrictEq(error.message, 'some problem');
+    assertStrictEq(error.response.status, 501);
+    assertEquals(error.response.body, {
+        error   : 'Not Implemented',
+        message : 'some problem',
+        status  : 501
+    });
+});
+
+test('new Bang() with error', () => {
+    const error = new bang.Bang(new Error('some problem'));
+    assert(error instanceof Error);
+    assertStrictEq(error.message, 'some problem');
+    assertStrictEq(error.response.status, 500);
+    assertEquals(error.response.body, {
+        error   : 'Internal Server Error',
+        message : 'Internal Server Error',
+        status  : 500
+    });
+});
+
+test('new Bang() with error and 501 status', () => {
+    const error = new bang.Bang(new Error('some problem'), { status : 501 });
+    assert(error instanceof Error);
+    assertStrictEq(error.message, 'some problem');
+    assertStrictEq(error.response.status, 501);
+    assertEquals(error.response.body, {
+        error   : 'Not Implemented',
+        message : 'some problem',
+        status  : 501
     });
 });
