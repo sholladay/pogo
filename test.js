9e684daf1196d6d04f51631bf9c897f6159caf0c
Seth Holladay <me@seth-holladay.com>
2020-02-17 16:41:23 -0500

    Use `deno test` for testing

--- test.js
+++
@@ -1,8 +0,0 @@
-import './test/bang.js';
-import './test/request.js';
-import './test/response.js';
-import './test/router.js';
-import './test/server.jsx';
-import './test/toolkit.js';
-
-await Deno.runTests();
