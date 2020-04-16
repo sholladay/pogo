32f46257f196e2ceff9763f5a1bb8b9151ada330
Seth Holladay <me@seth-holladay.com>
2020-04-15 22:53:18 -0400

    Fix style nits

--- example/simple-server/dev-dependencies.js
+++ example/simple-server/dev-dependencies.js
@@ -1,6 +1,3 @@
-import {
-    assertEquals,
-    assertStrictEq,
-} from "https://deno.land/std@v0.40.0/testing/asserts.ts";
+import { assertEquals, assertStrictEq } from "https://deno.land/std@v0.40.0/testing/asserts.ts";
 
 export { assertEquals, assertStrictEq };
