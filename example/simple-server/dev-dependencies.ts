e3c7bbd932eab7943b31b9d6120d637b18062b42
patlehmann1 <patrick.lehmann@techs-MacBook-Pro-2.local>
2020-05-09 10:22:22 -0400

    updated deno std dependency to 0.50.0

--- example/simple-server/dev-dependencies.ts
+++ example/simple-server/dev-dependencies.ts
@@ -1,4 +1,4 @@
-import { assertEquals, assertStrictEq } from 'https://deno.land/std@v0.41.0/testing/asserts.ts';
+import { assertEquals, assertStrictEq } from 'https://deno.land/std@v0.50.0/testing/asserts.ts';
 
 export {
     assertEquals,
