c07bfa87a56c9e6d300c7db16c55d7bf35e5a951
Seth Holladay <me@seth-holladay.com>
2020-06-11 13:10:28 -0400

    Add h.file() for serving static files

--- lib/toolkit.ts
+++ lib/toolkit.ts
@@ -1,9 +1,18 @@
 import Response from './response.ts';
-
-type JSONStringifyable = boolean | null | number | object | string;
-type ResponseBody = Deno.Reader | Uint8Array | JSONStringifyable;
+import { ResponseBody } from './types.ts';
+import { mime } from '../dependencies.ts';
 
 export default class Toolkit {
+    async file(path: string): Promise<Response> {
+        const file = await Deno.readFile(path);
+        const mediaType = mime.lookup(path);
+        const contentType = mime.contentType(mediaType || '');
+        const response = this.response(file);
+        if (contentType) {
+            response.type(contentType);
+        }
+        return response;
+    }
     response(body?: ResponseBody): Response {
         return new Response({ body });
     }
