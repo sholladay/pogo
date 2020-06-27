75aa5b3c7c7264d2bf9c86cf81eec9997893be59
Seth Holladay <me@seth-holladay.com>
2020-06-27 19:09:46 -0400

    Add confine option for h.file()
    Defaults to the current working directory.

--- dependencies.ts
+++ dependencies.ts
@@ -4,6 +4,7 @@ import * as cookie from 'https://deno.land/std@v0.56.0/http/cookie.ts';
 import * as http from 'https://deno.land/std@v0.56.0/http/server.ts';
 import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@v0.56.0/http/http_status.ts';
 import * as mime from 'https://cdn.pika.dev/mime-types@2.1.27';
+import * as path from 'https://deno.land/std@v0.56.0/path/mod.ts';
 
 export {
     React,
@@ -11,6 +12,7 @@ export {
     cookie,
     http,
     mime,
+    path,
     status,
     statusText
 };
