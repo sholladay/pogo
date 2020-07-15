3986bedc6e1ec2b9b44e87b53fc9ddfbbd2486c9
Adam Faur <adam.faur@gmail.com>
2020-07-15 00:05:21 -0700

    provide named exports for server and router
    remove main namespace export

--- main.ts
+++ main.ts
@@ -7,13 +7,12 @@ export * as types from './lib/types.ts'
 export { default as Router } from './lib/router.ts';
 export { default as Server } from './lib/server.ts';
 
-export const Main = {
-    server(options: ServerOptions): Server {
-        return new Server(options);
-    },
-    router(route?: RoutesList, options?: RouteOptions | RouteHandler, handler?: RouteHandler): Router {
-        return new Router(route, options, handler);
-    }
-};
+export const server = (options: ServerOptions): Server => {
+    return new Server(options);
+}
 
-export default Main;
+export const router = (route?: RoutesList, options?: RouteOptions | RouteHandler, handler?: RouteHandler): Router => {
+    return new Router(route, options, handler);
+}
+
+export default { server, router };
