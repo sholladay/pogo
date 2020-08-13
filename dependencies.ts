bc321bd2b03e4221ea733cbb284ea1821ea313b1
Seth Holladay <me@seth-holladay.com>
2020-08-12 21:31:41 -0400

    Update to std@v0.61.0 and test Deno v1.2.3

--- dependencies.ts
+++ dependencies.ts
@@ -1,11 +1,11 @@
 import React from 'https://cdn.pika.dev/react@16.13.1';
 // @deno-types="https://github.com/soremwar/deno_types/raw/4a5066025e84d2c5353d0f40a8869c65d7c82734/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from 'https://jspm.dev/react-dom@16.13.1/server';
-import * as cookie from 'https://deno.land/std@v0.61.0/http/cookie.ts';
-import * as http from 'https://deno.land/std@v0.61.0/http/server.ts';
-import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@v0.61.0/http/http_status.ts';
+import * as cookie from 'https://deno.land/std@v0.64.0/http/cookie.ts';
+import * as http from 'https://deno.land/std@v0.64.0/http/server.ts';
+import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@v0.64.0/http/http_status.ts';
 import * as mime from 'https://cdn.pika.dev/mime-types@2.1.27';
-import * as path from 'https://deno.land/std@v0.61.0/path/mod.ts';
+import * as path from 'https://deno.land/std@v0.64.0/path/mod.ts';
 
 export {
     React,
