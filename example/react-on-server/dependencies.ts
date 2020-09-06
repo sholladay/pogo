9495bb52c90c3316fcc65eb0eef2207e734a84a5
Zane Milakovic <zane.milakovic@outlook.com>
2020-09-05 19:32:23 -0500

    Update dependencies.ts

--- example/react-on-server/dependencies.ts
+++ example/react-on-server/dependencies.ts
@@ -1,5 +1,6 @@
 import pogo from 'https://deno.land/x/pogo/main.ts';
-import React from 'https://cdn.pika.dev/react@16.13.1';
+// @deno-types="https://github.com/soremwar/deno_types/raw/4a5066025e84d2c5353d0f40a8869c65d7c82734/react/v16.13.1/react.d.ts"
+import React from 'https://jspm.dev/react@16.13.1';
 
 export {
     pogo,
