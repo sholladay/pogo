8e47a1706e4c6b1d2bbe8e9bf8e1fcd85088281b
Seth Holladay <me@seth-holladay.com>
2020-09-07 11:24:41 -0400

    Fix loading of type definitions for mime-types

--- dependencies.ts
+++ dependencies.ts
@@ -5,7 +5,7 @@ import ReactDOMServer from 'https://jspm.dev/react-dom@16.13.1/server';
 import * as cookie from 'https://deno.land/std@0.64.0/http/cookie.ts';
 import * as http from 'https://deno.land/std@0.64.0/http/server.ts';
 import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@0.64.0/http/http_status.ts';
-import * as mime from 'https://jspm.dev/mime-types@2.1.27';
+import * as mime from 'https://cdn.pika.dev/mime-types@2.1.27';
 import * as path from 'https://deno.land/std@0.64.0/path/mod.ts';
 
 export {
