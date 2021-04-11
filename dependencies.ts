c4b0316131b6650fc7acfe9534afb69d1a664516
Seth Holladay <me@seth-holladay.com>
2021-04-10 22:46:13 -0400

    Update dependencies

--- dependencies.ts
+++ dependencies.ts
@@ -2,10 +2,10 @@
 import React from 'https://jspm.dev/react@16.13.1';
 // @deno-types="https://raw.githubusercontent.com/soremwar/deno_types/b5a146610e2c97c1612371fcf610b541f950ee73/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from 'https://jspm.dev/react-dom@16.13.1/server';
-import * as cookie from 'https://deno.land/std@0.87.0/http/cookie.ts';
-import * as http from 'https://deno.land/std@0.87.0/http/server.ts';
-import * as path from 'https://deno.land/std@0.87.0/path/mod.ts';
-import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@0.87.0/http/http_status.ts';
+import * as cookie from 'https://deno.land/std@0.92.0/http/cookie.ts';
+import * as http from 'https://deno.land/std@0.92.0/http/server.ts';
+import * as path from 'https://deno.land/std@0.92.0/path/mod.ts';
+import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@0.92.0/http/http_status.ts';
 import * as mime from 'https://cdn.skypack.dev/mime-types@2.1.28?dts';
 
 export {
