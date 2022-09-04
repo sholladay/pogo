6f6ee9076eab0943c300952c1e24d4bece1b7485
Seth Holladay <me@seth-holladay.com>
2022-09-04 16:18:00 -0400

    Update to std@0.154.0

--- dependencies.ts
+++ dependencies.ts
@@ -1,10 +1,10 @@
 import React from 'https://esm.sh/react@18.2.0';
 import ReactDOMServer from 'https://esm.sh/react-dom@18.2.0/server';
-import * as cookie from 'https://deno.land/std@0.132.0/http/cookie.ts';
-import * as http from 'https://deno.land/std@0.132.0/http/server.ts';
-import * as path from 'https://deno.land/std@0.132.0/path/mod.ts';
-import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@0.132.0/http/http_status.ts';
-import * as streams from 'https://deno.land/std@0.132.0/streams/conversion.ts';
+import * as cookie from 'https://deno.land/std@0.154.0/http/cookie.ts';
+import * as http from 'https://deno.land/std@0.154.0/http/server.ts';
+import * as path from 'https://deno.land/std@0.154.0/path/mod.ts';
+import { Status as status, STATUS_TEXT as statusText } from 'https://deno.land/std@0.154.0/http/http_status.ts';
+import * as streams from 'https://deno.land/std@0.154.0/streams/conversion.ts';
 import * as mime from 'https://esm.sh/mime-types@2.1.35';
 
 export {
