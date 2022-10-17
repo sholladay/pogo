cd822c758883aca12d2061eac61b31f86333524f
Seth Holladay <me@seth-holladay.com>
2022-10-17 12:54:36 -0400

    Update test.js

--- example/react-on-client/test.js
+++ example/react-on-client/test.js
@@ -4,23 +4,24 @@ import server from './main.ts';
 const { test } = Deno;
 
 test('GET /', async () => {
-    const response = await server.inject({
-        method : 'GET',
-        url    : '/'
-    });
+    const response = await server.inject('/');
     assertStrictEq(response.status, 200);
     assertStrictEq(response.headers.get('content-type'), 'text/html; charset=utf-8');
-    assertStrictEq(new TextDecoder().decode(response.body),
+    assertStrictEq(await response.text(),
 `<!DOCTYPE html>
-<html lang=\"en\">
-<head>
-    <meta charset=\"UTF-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>Counter</title>
-    <script type="module" src=\"/app.js\"></script>
-</head>
-<body>
-    <div id="app"></div>
-</body>
-</html>`);
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pogo Example | React on Client</title>
+        <meta name="viewport" content=\"width=device-width, initial-scale=1.0\">
+        <script type="module" src=\"/app.js\"></script>
+    </head>
+    <body>
+        <p>This is an example Pogo app with React client-side rendering.</p>
+        <p>Click the buttons below.</p>
+        <div id="app"></div>
+    </body>
+</html>
+`
+    );
 });
